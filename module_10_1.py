7d366313e1d6f9d3370b29dac87d05643b5828c7
rubiCON00 <zevs9905@gmail.com>
2024-11-26 22:31:29 +0300

    Update module_10_1.py

--- module_10_1.py
+++ module_10_1.py
@@ -1,4 +1,5 @@
 from time import sleep
+from datetime import datetime
 from threading import Thread
 def write_words(word_count, file_name):
     file = open(file_name, 'a', encoding='utf-8')
@@ -8,12 +9,17 @@ def write_words(word_count, file_name):
     file.close()
     print(f'Завершилась запись в файл {file_name}')
 
+time_start = datetime.now()
 
 write_words(10, 'example1.txt')
 write_words(30, 'example2.txt')
 write_words(200, 'example3.txt')
 write_words(100, 'example4.txt')
+time_stop = datetime.now()
+time_res = time_stop - time_start
+print(f'Время работы функций {time_res}')
 
+time2_start = datetime.now()
 
 thr_first = Thread(target = write_words, args = (10, 'example5.txt'))
 thr_second = Thread(target = write_words, args = (30, 'example6.txt'))
@@ -29,3 +35,8 @@ thr_first.join()
 thr_second.join()
 thr_third.join()
 thr_fourh.join()
+
+time2_stop = datetime.now()
+time2_res = time2_stop - time2_start
+print(f'Время работы потоков {time2_res}')
+print(f'Использование Потоков быстрее функций на {time_res-time2_res} секунд')
